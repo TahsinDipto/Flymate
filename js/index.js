41d8f2df62abc873e2048ffed1cb387fe9b02ed0
TahsinDipto <tahsin8511@gmail.com>
2025-09-18 21:27:17 +0600

    flymate project complete no responsive

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const announce = document.querySelector('.dark-announce');
+const button = document.querySelector('.dark-button');
+
+function dark() {
+    if(button.style.backgroundColor =='#202020'){
+
+        button.style.backgroundColor = '#f3f4f6'
+    }
+    else{
+        button.style.backgroundColor = '#202020'
+    }
+
+    button.style.backgroundColor = '#f3f4f6'
+
+
+    // if(input1.value === ""){
+    //     alert.innerHTML="Please input a number"
+    // }else{
+    //     p1.innerHTML="Please click this button who will play at first"
+    //     h2.innerHTML=""
+    //     inputs.style.display="none"
+    //     inputs2.style.display= "flex"
+    //     alert.innerHTML=""
+    //     icon1.style.display="none"
+    // }
+};
+
